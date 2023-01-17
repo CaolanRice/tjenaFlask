97d95cb9d5ecdd79ea7defba8512b7ae79338e4b
CaolanRice <c_f_rice@hotmail.co.uk>
2023-01-17 21:34:28 +0100

    creating the update and delete views

--- tjenaFlask/blog.py
+++ tjenaFlask/blog.py
@@ -43,3 +43,63 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+'''The check_author argument is defined so that the function can be used to get a post without checking the author. 
+This would be useful if you wrote a view to show an individual post on a page, where the user doesn’t matter because they’re not modifying the post.'''
+
+def get_post(id, check_author=True): #fetches post by id and checks if author matches logged in user
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
+'''The update function takes id as an arg. That corresponds to the <int:id> in the route. URL will look like /1/update. Flask will capture the 1, ensure it’s an int, 
+and pass it as the id argument. If you don’t specify int: and instead do <id>, it will be a string. To generate a URL to the update page, url_for() needs to be 
+passed the id so it knows what to fill in: url_for('blog.update', id=post['id']).'''
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
